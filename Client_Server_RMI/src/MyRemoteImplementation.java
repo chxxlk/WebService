84ca3ea1871a1c355dc5200c3236841f1ba4d1c7
Chris Stevanus Lekpey <chrislekpey06@gmail.com>
2026-04-29 20:03:24 +0700

    feat: multiple users using HashMap.

--- Client_Server_RMI/src/MyRemoteImplementation.java
+++ Client_Server_RMI/src/MyRemoteImplementation.java
@@ -1,13 +1,20 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyRemoteImplementation extends UnicastRemoteObject implements MyRemoteInterface {
-  // TODO: using array for multiple user
-  private final String VALID_USER = "chris";
-  private final String VALID_PASS = "123";
+  // private final String VALID_USER = "chris";
+  // private final String VALID_PASS = "123";
+
+  private Map<String, String> users = new HashMap<>();
 
   protected MyRemoteImplementation() throws RemoteException {
     super();
+    // dummy
+    users.put("chris", "123");
+    users.put("juan", "123");
+    users.put("stev", "123");
   }
 
   @Override
@@ -18,19 +25,13 @@ public class MyRemoteImplementation extends UnicastRemoteObject implements MyRem
     return "Hello, " + name + "!";
   }
 
-  // @Override
-  // public String auth(String username, String password) throws RemoteException {
-  // if (!authenticate(username, password)) {
-  // throw new SecurityException("Unathorized access");
-  // }
-  // }
-
   @Override
   public String info(int height, int weight) throws RemoteException {
     return "\nTinggi : " + height + "cm" + "\nBerat : " + weight + "kg";
   }
 
   private boolean authenticate(String name, String password) {
-    return VALID_USER.equals(name) && VALID_PASS.equals(password);
+    // return VALID_USER.equals(name) && VALID_PASS.equals(password);
+    return users.containsKey(name) && users.get(name).equals(password);
   }
 }
